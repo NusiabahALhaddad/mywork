86d5c84ef8b52888a1f91cd4288884d3603677af
abisonah <eng.nusibah@gmail.com>
2024-02-13 17:02:39 +0300

    inti project

---
+++ src/main/java/com/example/mytastproject/MyTastProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytastproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTastProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTastProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mytastproject/MyTastProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytastproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTastProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
